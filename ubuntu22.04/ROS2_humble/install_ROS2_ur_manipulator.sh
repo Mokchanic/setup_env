0bff23908e40170745fd4f3192e19aa41c1c0a65
Mokchanic <mokok118@kakao.com>
2024-06-07 17:38:52 +0900

    update ur_setup

--- ubuntu22.04/ROS2_humble/install_ROS2_ur_manipulator.sh
+++ ubuntu22.04/ROS2_humble/install_ROS2_ur_manipulator.sh
@@ -7,22 +7,17 @@ sudo apt-get -y install ros-${ROS_DISTRO}-ur-robot-driver
 sudo apt -y install python3-colcon-common-extensions python3-vcstool
 
 # Make workspace
-export COLCON_WS=~/workspace/ros_ur_driver
+export COLCON_WS=~/workspace/ur_ws
 mkdir -p $COLCON_WS/src
+cd $COLCON_WS
 
-git clone https://github.com/UniversalRobots/Universal_Robots_ROS2_Driver.git src/Universal_Robots_ROS2_Driver
+git clone -b humble https://github.com/UniversalRobots/Universal_Robots_ROS2_Driver.git src/Universal_Robots_ROS2_Driver
 vcs import src --skip-existing --input src/Universal_Robots_ROS2_Driver/Universal_Robots_ROS2_Driver.${ROS_DISTRO}.repos
 
+rosdep init
+rosdep update
 rosdep install --ignore-src --from-paths src -y
 colcon build --cmake-args -DCMAKE_BUILD_TYPE=Release
+
 source install/setup.bash
-
-sudo apt -y install ros-humble-gazebo-ros2-control
-
-git clone https://github.com/UniversalRobots/Universal_Robots_ROS2_Gazebo_Simulation.git src/Universal_Robots_ROS2_Gazebo_Simulation
-vcs import src --input src/Universal_Robots_ROS2_Gazebo_Simulation/Universal_Robots_ROS2_Gazebo_Simulation.${ROS_DISTRO}.repos
-rosdep install --ignore-src --from-paths src -y
-
-colcon build
-source install/setup.bash
-
+echo 'source ~/workspace/ur_ws/install/local_setup.bash' >> ~/.bashrc
