4bb117e8a3403535c8a78997f69abe67b84dd704
Mokchanic <mokok118@kakao.com>
2024-07-31 14:48:37 +0900

    add cd

--- setting_docker_ubuntu22.04.sh
+++ setting_docker_ubuntu22.04.sh
@@ -7,6 +7,7 @@
 echo "Setup Ubuntu 22.04 ..."
 cd ubuntu22.04/setup_programs
 ./setup_bashrc.sh
+cd .. && cd ROS2_humble
 ./install_ROS2_humble_full.sh
 ./install_ROS2_moveit2.sh
 ./install_ROS2_ur_gazebo.sh
