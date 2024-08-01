e2f9568208241eb6233102329781a5e1ad3e89e1
Mokchanic <mokok118@kakao.com>
2024-08-01 15:21:39 +0900

    update setting_docker

--- setting_docker_ubuntu22.04.sh
+++ setting_docker_ubuntu22.04.sh
@@ -10,8 +10,9 @@ cd ubuntu22.04/setup_programs
 
 cd .. && cd ROS2_humble
 ./install_ROS2_humble_full.sh
-./install_ROS2_moveit2.sh
-./install_ROS2_ur_gazebo.sh
-./install_ROS2_ur_manipulator.sh
+./install_ROS2_realsense.sh
+# ./install_ROS2_moveit2.sh
+# ./install_ROS2_ur_gazebo.sh
+# ./install_ROS2_ur_manipulator.sh
 
 echo "Now reboot system to apply changes"
